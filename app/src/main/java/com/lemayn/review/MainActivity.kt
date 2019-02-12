2330b1305666dd87905d7857357998fbf7707587
liyang <liyang@qgutech.com>
2019-02-12 14:36:26 +0800

    lifecycle log

--- app/src/main/java/com/lemayn/review/MainActivity.kt
+++ app/src/main/java/com/lemayn/review/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.lemayn.review
 
-import android.support.v7.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.i("onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Timber.i("onRestoreInstanceState")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putString("method", "onSaveInstanceState")
+        Timber.i("onSaveInstanceState")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        Timber.i("onConfigurationChanged")
+    }
+
 }
