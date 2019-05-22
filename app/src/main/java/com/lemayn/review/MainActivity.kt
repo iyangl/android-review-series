4ad24454b3aec357e3c95a0d1aacc326b7009636
liyang <www.798220773@qq.com>
2019-05-22 08:32:45 +0800

    okhttp源码

--- app/src/main/java/com/lemayn/review/MainActivity.kt
+++ app/src/main/java/com/lemayn/review/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lemayn.review
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        OkHttpClient()
     }
 }
