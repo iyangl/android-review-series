4ced630e46b8844dd51becd1b2324e30a5d6d083
liyang <www.798220773@qq.com>
2019-05-27 16:24:59 +0800

    retrofit2

---
+++ lib_square/src/main/java/retrofit2/KotlinExtensions.kt
@@ -0,0 +1,7 @@
+// IntelliJ API Decompiler stub source generated from a class file
+// Implementation of methods is not available
+
+package retrofit2
+
+public inline fun <reified T> retrofit2.Retrofit.create() { /* compiled code */ }
+
