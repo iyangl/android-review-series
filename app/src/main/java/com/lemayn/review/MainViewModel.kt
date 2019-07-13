7e386549285541ffeb793ffab467721f2dc7f3dc
liyang <www.798220773@qq.com>
2019-07-13 14:16:28 +0800

    viewmodel databinding

---
+++ app/src/main/java/com/lemayn/review/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.lemayn.review
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * author: ly
+ * date  : 2019/7/13 13:50
+ * desc  :
+ */
+class MainViewModel : ViewModel() {
+
+
+
+}
